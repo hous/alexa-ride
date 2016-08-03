057d19945bfb9c834d0d09d9e8cfeb262781769c
Adam Housman <hous27@gmail.com>
2016-08-03 17:05:56 -0400

    Prod mode. This should not be in the code.

--- apps/bikeride/database_helper.js
+++ apps/bikeride/database_helper.js
@@ -12,9 +12,9 @@ var localCredentials = {
   secretAccessKey: config.secretAccessKey
 };
 var localDynasty = require("dynasty")(localCredentials, localUrl);
-var dynasty = localDynasty;
+//var dynasty = localDynasty;
 //comment line 13 and uncomment line 15 for production
-//var dynasty = require("dynasty")({});
+var dynasty = require("dynasty")({});
 
 function DatabaseHelper() {
 }
